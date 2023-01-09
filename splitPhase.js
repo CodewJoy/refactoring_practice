f24916f7e5ca05253e3c920696ade1daa01fbf4d
Joy Hung <yichen.hung6699@gmail.com>
2023-01-09 22:03:14 +0800

    splitPhase_way_of_joy

--- splitPhase.js
+++ splitPhase.js
@@ -9,25 +9,27 @@ const shippingMethod = {
     discountThreshold: 50,
     discountedFee: 1
 };
+
+// 最終價格
 function priceOrder(product, quantity, shippingMethod) {
     const priceData = calculatePricingData(product, quantity);
-    return applyShipping(priceData, shippingMethod);
+    const shippingCost = applyShipping(priceData, shippingMethod);
+    return priceData.basePrice - priceData.discount + shippingCost;
 }
 // 使用產品資訊計算產品價格
 function calculatePricingData(product, quantity) {
     const basePrice = product.basePrice * quantity;
     const discount = Math.max(quantity - product.discountThreshold, 0)
             * product.basePrice * product.discountRate;
-    return { basePrice, quantity, discount };
+    return { basePrice, quantity, discount }; // priceData interface
 }
+ // 使用貨運資訊計算運費
 function applyShipping(priceData, shippingMethod) {
-    // 使用貨運資訊計算運費
+   
     const shippingPerCase = (priceData.basePrice > shippingMethod.discountThreshold)
             ? shippingMethod.discountedFee : shippingMethod.feePerCase; 
     const shippingCost = priceData.quantity * shippingPerCase;
-    // 最終價格
-    const price =  priceData.basePrice - priceData.discount + shippingCost;
-    return price;
+    return shippingCost;
 }
 const price = priceOrder(product, quantity, shippingMethod);
 console.log('price', price);
