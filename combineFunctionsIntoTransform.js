1a5ef1a1dda244f8db7b4c96d4800c7d35ed3414
Joy Hung <yichen.hung6699@gmail.com>
2023-01-11 00:30:36 +0800

    combineFuncIntoTransform__example_origin

---
+++ combineFunctionsIntoTransform.js
@@ -0,0 +1,17 @@
+const reading = {customer: "ivan", quantity: 10, month: 5, year: 2017};
+
+// client 1
+const aReading1 = acquireReading();
+const baseCharge = baseRate(aReading.month, aReading.year) * aReading.quantity;
+
+// client 2
+const aReading2 = acquireReading();
+const base = (baseRate(aReading.month, aReading.year) * aReading.quantity);
+const taxableCharge =  Math.max(0, base - taxThreshold(aReading.year));
+
+// client 3
+const aReading = acquireReading();
+const basicChargeAmount = calculateBaseCharge(aReading);
+function calculateBaseCharge(aReading) {
+    return  baseRate(aReading.month, aReading.year) * aReading.quantity;
+}
---
+++ utils/index.js
@@ -0,0 +1,14 @@
+export function deepClone(obj) {
+    if (obj === null || typeof obj !== 'object') {
+      return obj;
+    }
+  
+    let clone = Array.isArray(obj) ? [] : {};
+    for (const key in obj) {
+      if (Object.prototype.hasOwnProperty.call(obj, key)) {
+        clone[key] = deepClone(obj[key]);
+      }
+    }
+  
+    return clone;
+}
