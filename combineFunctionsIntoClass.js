de00807116e57adfe761da3564db5d8c44e86e66
Joy Hung <yichen.hung6699@gmail.com>
2023-01-15 14:43:30 +0800

    combineFuncIntoClass_encapsulate_record

--- combineFunctionsIntoClass.js
+++ combineFunctionsIntoClass.js
@@ -21,13 +21,13 @@ function acquireReading() {
 // client 1: 計算該使用者在某年某月使用的茶飲數量所必須繳的稅額（茶在英國視為生活必需品，必須繳稅）
 const aReading1 = acquireReading();
 const baseCharge = baseRate(aReading1.month, aReading1.year) * aReading1.quantity;
-console.log('baseCharge', baseCharge);
+console.log('baseCharge', baseCharge); // test result: 20
 
 // client 2: taxableCharge: 計算法規允許的基本免稅額
 const aReading2 = acquireReading();
 const base = (baseRate(aReading2.month, aReading2.year) * aReading2.quantity);
 const taxableCharge =  Math.max(0, base - taxThreshold(aReading2.year));
-console.log('taxableCharge', taxableCharge);
+console.log('taxableCharge', taxableCharge); // test result: 0
 
 // client 3: 在 code base 中其他地方，寫了跟 client 1 一樣的邏輯，並且使用了 Extract Function
 const aReading3 = acquireReading();
@@ -38,7 +38,19 @@ function calculateBaseCharge(aReading) {
 function calculateBaseCharge(aReading) {
     return  baseRate(aReading.month, aReading.year) * aReading.quantity;
 }
-console.log('basicChargeAmount', basicChargeAmount);
+console.log('basicChargeAmount', basicChargeAmount); // test result: 20
+class Reading {
+    constructor(data) {
+      this._customer = data.customer;
+      this._quantity = data.quantity;
+      this._month = data.month;
+      this._year = data.year;
+    }
+    get customer() {return this._customer;}
+    get quantity() {return this._quantity;}
+    get month()    {return this._month;}
+    get year()     {return this._year;}
+}
 
 /** Following funct is assumed by Joy cause in the book has no related example */
 function baseRate(month, year) {
@@ -65,3 +77,4 @@ function deepClone(obj) {
     }
     return clone;
 }
+
